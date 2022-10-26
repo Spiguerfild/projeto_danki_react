08241de482445914703899b12a73d88c4148d34c
Luis Henrique <luique16@outlook.com>
2022-10-26 17:26:06 -0400

    hook de efeito e de estado

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import React, { useState, useEffect } from 'react';
 
-import './App.css';
-import Header from './Header.js';
+ export default function App() {
+  const [count, setCount] = useState(0);
 
+  // Similar ao componentDidMount e componentDidUpdate:
+  useEffect(() => {
+    // Atualiza o título do documento usando a API do browser
+    document.title = `Você clicou ${count} vezes`;
+  });
 
-function App(props) {
   return (
-    <div className="App">
- 
+    <div>
+      <p>Você clicou {count} vezes</p>
+      <button onClick={() => setCount(count + 1)}>
+        Clique aqui
+      </button>
     </div>
   );
 }
-
-export default App;
