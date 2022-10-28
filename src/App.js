476ad10cfc941a3b82834ce39f6ba9713223c5bd
Luis Henrique <luique16@outlook.com>
2022-10-28 17:27:37 -0400

    react na pratica fim modulo

--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
 import React, { useState, useEffect } from "react";
-import './App.css'
+import "./App.css";
+import axios from "axios";
+
 export default function App() {
-  const [hora, setHora] = useState(4);
 
-  const [minuto, setMinuto] = useState(59);
+const[nomes,setNomes] = useState([])
 
-  const [segundo, setSegundo] = useState(50);
+ 
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setSegundo(segundo + 1);
-
-    if(segundo == 59){
-      setSegundo(0);
-      setMinuto(minuto+1)
-
-      if(minuto == 59){
-        setMinuto(0)
-        setHora(hora+1)
-      }
-    }
   
-    }, 1000);
+axios.get('https://jsonplaceholder.typicode.com/users').then(res => {
+  console.log(res.data)
+  setNomes(res.data);
+})
 
-    return () => clearInterval(interval);
   });
 
   return (
     <div className="tempo">
-      <h2>
-        {hora}:{minuto}:{segundo}
-      </h2>
+      {
+  nomes.map(function(data){
+return(
+  <div> 
+  <h2>{data.name} | {data.email}</h2>
+  </div>
+)
+  })
+      }
     </div>
   );
 }
